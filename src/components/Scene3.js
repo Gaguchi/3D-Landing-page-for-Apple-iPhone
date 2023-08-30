8e50f11b4c4d8f31b833426338f7cf4119c7c810
Gaguchi <gaga9393@gmail.com>
2023-08-31 00:39:27 +0400

    first commit

--- src/components/Scene3.js
+++ src/components/Scene3.js
@@ -12,13 +12,13 @@ import { useThree } from "@react-three/fiber";
 
 export default function Model3({ ...props }) {
   const group = useRef();
-  const { nodes, materials } = useGLTF("/scene.gltf");
+  const { nodes, materials } = useGLTF("/Gio.gltf");
 
   let camera = useThree((state) => state.camera);
 
   useLayoutEffect(() => {
     camera.position.set(-0.1,0.4,5);
-    materials.Body.color.set("#9BB5CE");
+    //materials.Body.color.set("#9BB5CE");
 
     if(window.matchMedia("(max-width: 48em)").matches){
       camera.fov = 18;
@@ -27,132 +27,34 @@ export default function Model3({ ...props }) {
         
   }, []);
 
+  
   return (
-    <group ref={group} {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
-          <group scale={100}>
-            <mesh
-              geometry={nodes.Body_Mic_0.geometry}
-              material={materials.material}
-            />
-            <mesh
-              geometry={nodes.Body_Bezel_0.geometry}
-              material={materials.Bezel}
-            />
-            <mesh
-              geometry={nodes.Body_Body_0.geometry}
-              material={materials.Body}
-            />
-            <mesh
-              geometry={nodes.Body_Wallpaper_0.geometry}
-              material={materials.Wallpaper}
-            />
-            <mesh
-              geometry={nodes.Body_Camera_Glass_0.geometry}
-              material={materials.Camera_Glass}
-            />
-            <mesh
-              geometry={nodes.Body_Lens_0.geometry}
-              material={materials.Lens}
-            />
-            <mesh
-              geometry={nodes.Body_Material_0.geometry}
-              material={materials.Material}
-            />
-            <mesh
-              geometry={nodes.Camera_Body_0.geometry}
-              material={materials.Body}
-            />
-            <mesh
-              geometry={nodes.Camera_Glass_0.geometry}
-              material={materials.Glass}
-            />
-            <mesh
-              geometry={nodes.Camera_Camera_Frame001_0.geometry}
-              material={materials["Camera_Frame.001"]}
-            />
-            <mesh
-              geometry={nodes.Camera_Mic_0.geometry}
-              material={materials.material}
-            />
-            <mesh
-              geometry={nodes.Body001_Screen_Glass_0.geometry}
-              material={materials.Screen_Glass}
-            />
-            <mesh
-              geometry={nodes.Button_Frame_0.geometry}
-              material={materials.Frame}
-            />
-            <mesh
-              geometry={nodes.Circle003_Frame_0.geometry}
-              material={materials.Frame}
-            />
-            <mesh
-              geometry={nodes.Apple_Logo_Logo_0.geometry}
-              material={materials.Logo}
-            />
-            <mesh
-              geometry={nodes.Camera001_Body_0.geometry}
-              material={materials.Body}
-            />
-            <mesh
-              geometry={nodes.Camera001_Gray_Glass_0.geometry}
-              material={materials.Gray_Glass}
-            />
-            <mesh
-              geometry={nodes.Camera001_Flash_0.geometry}
-              material={materials.Flash}
-            />
-            <mesh
-              geometry={nodes.Camera001_Port_0.geometry}
-              material={materials.Port}
-            />
-            <mesh
-              geometry={nodes.Camera001_Camera_Frame_0.geometry}
-              material={materials.Camera_Frame}
-            />
-            <mesh
-              geometry={nodes.Camera001_Camera_Glass_0.geometry}
-              material={materials.Camera_Glass}
-            />
-            <mesh
-              geometry={nodes.Camera001_Lens_0.geometry}
-              material={materials.Lens}
-            />
-            <mesh
-              geometry={nodes.Camera001_Black_Glass_0.geometry}
-              material={materials.Black_Glass}
-            />
-            <mesh
-              geometry={nodes.Camera003_Material002_0.geometry}
-              material={materials["Material.002"]}
-            />
-            <mesh
-              geometry={nodes.Frame_Frame_0.geometry}
-              material={materials.Frame}
-            />
-            <mesh
-              geometry={nodes.Frame_Frame2_0.geometry}
-              material={materials.Frame2}
-            />
-            <mesh
-              geometry={nodes.Frame_Port_0.geometry}
-              material={materials.Port}
-            />
-            <mesh
-              geometry={nodes.Frame_Antenna_0.geometry}
-              material={materials.Antenna}
-            />
-            <mesh
-              geometry={nodes.Frame_Mic_0.geometry}
-              material={materials.material}
-            />
-          </group>
-        </group>
+    <group {...props} dispose={null}>
+      <group position={[0.529, 1.887, 0.136]} rotation={[Math.PI / 2, 0, 0]} scale={[0.728, 0.458, 1.855]}>
+        <mesh geometry={nodes.Circle_1.geometry} material={materials['Material.001']} />
+        <mesh geometry={nodes.Circle_2.geometry} material={materials['Material.006']} />
+        <mesh geometry={nodes.Circle_3.geometry} material={materials.Material} />
       </group>
+      <mesh geometry={nodes.Cylinder001.geometry} material={materials['Material.009']} position={[0.839, 3.435, -0.13]} rotation={[-Math.PI / 2, 0, 0]} scale={0.024} />
+      <mesh geometry={nodes.Cylinder002.geometry} material={materials['Material.009']} position={[0.218, 3.435, -0.13]} rotation={[-Math.PI / 2, -0.641, 0]} scale={0.024} />
+      <mesh geometry={nodes.Cylinder003.geometry} material={materials['Material.009']} position={[0.541, 3.649, -0.13]} rotation={[-Math.PI / 2, 0.751, 0]} scale={0.024} />
+      <mesh geometry={nodes.Cylinder004.geometry} material={materials['Material.009']} position={[1.022, 0.25, -0.13]} rotation={[-Math.PI / 2, 0, 0]} scale={0.024} />
+      <mesh geometry={nodes.Cylinder005.geometry} material={materials['Material.009']} position={[0.085, 0.25, -0.13]} rotation={[-Math.PI / 2, -0.641, 0]} scale={0.024} />
+      <mesh geometry={nodes.Cylinder006.geometry} material={materials['Material.009']} position={[0.541, 0.083, -0.13]} rotation={[-Math.PI / 2, 0.751, 0]} scale={0.024} />
+      <group position={[-0.854, 0.848, 0.007]} scale={[0.232, 0.811, 0.232]}>
+        <mesh geometry={nodes.Cylinder_1.geometry} material={materials['Material.001']} />
+        <mesh geometry={nodes.Cylinder_2.geometry} material={materials['Material.002']} />
+        <mesh geometry={nodes.Cylinder_3.geometry} material={materials['Material.003']} />
+      </group>
+      <mesh geometry={nodes.Curve004.geometry} material={materials['SVGMat.001']} position={[-1.019, 1.668, 0.165]} scale={4.037} />
+      <mesh geometry={nodes.Curve.geometry} material={materials['SVGMat.001']} position={[-0.713, 7.088, 0.679]} scale={17.388} />
+      <mesh geometry={nodes.Curve001.geometry} material={materials['SVGMat.001']} position={[-0.7, 7.647, 0.679]} scale={17.388} />
+      <mesh geometry={nodes.Curve002.geometry} material={materials['SVGMat.001']} position={[-0.713, 8.024, 0.679]} scale={17.388} />
+      <mesh geometry={nodes.Curve003.geometry} material={materials['SVGMat.001']} position={[-0.704, 7.119, 0.679]} scale={17.388} />
+      <mesh geometry={nodes.Curve005.geometry} material={materials['Material.004']} position={[0.016, 1.556, 0.146]} rotation={[Math.PI / 2, 0, 0]} scale={13.031} />
+      <mesh geometry={nodes.Curve006.geometry} material={materials['Material.005']} position={[0.523, 2.684, 0.014]} rotation={[-Math.PI / 2, 0, 0]} scale={[-148.262, -21.942, -137.885]} />
     </group>
   );
 }
 
-useGLTF.preload("/scene.gltf");
+useGLTF.preload("/Gio.gltf");
